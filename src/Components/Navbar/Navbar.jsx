cc64011d7d4b54e2882354827f0f3702f32afeff
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-08-05 09:48:47 +0530

    Adding Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      
+        <div className="nav-logo">
+          <img src={logo} alt="Logo" />
+          <p>EverShop</p>
+        </div>
+        <ul className="nav-menu">
+          <li>Shop</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+        </ul>
+        <div className="nav-login-cart">
+          <button>Login</button>
+          <img src={cart_icon} alt="Cart" />
+        </div>
     </div>
   )
 }
