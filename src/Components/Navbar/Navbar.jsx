7a9618f4af1d7d02f0b1e6ffff4454d00290b8cb
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-08-06 07:40:13 +0530

    initializing new shop.jsx file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,9 @@ import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 export default function Navbar() {
+
+  const [menu, setMenu] = React.useState("shop");
+
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -12,10 +15,10 @@ export default function Navbar() {
           <p>EverShop</p>
         </div>
         <ul className="nav-menu">
-          <li>Shop <hr/></li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
+          <li onClick={() => {setMenu("shop")}} >Shop {menu === "shop"? <hr/>:<></>}</li>
+          <li onClick={() => {setMenu("mens")}} >Men {menu === "mens"? <hr/>:<></>}</li>
+          <li onClick={() => {setMenu("womens")}} >Women {menu === "womens"? <hr/>:<></>}</li>
+          <li onClick={() => {setMenu("kids")}} >Kids {menu === "kids"? <hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
           <button>Login</button>
